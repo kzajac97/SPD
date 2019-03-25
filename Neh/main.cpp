9782f7fdaaa1585f8c43484fd1d1aeb2410fa7b7
Krzysztof <kzajac1997@gmail.com>
2019-03-25 12:44:57 +0100

    Add modification to neh

--- Neh/main.cpp
+++ Neh/main.cpp
@@ -91,7 +91,21 @@ int maxspan_accelerated(std::vector<process> processes, unsigned int start_index
     return tasks.back().back(); 
 }
 
-// returns permutation with shortest task at the beginning
+// computes permutations of given process vector
+// <input> process vector 
+// <returns> 2D vector containing process objects
+// in every possible order (permutations)
+std::vector<std::vector<process> > getPermutations(std::vector<process> processes)
+{
+    std::vector<std::vector<process> > result;
+
+    do { result.push_back(processes); } 
+    while (std::next_permutation(processes.begin(),processes.end()));
+
+    return result;
+}
+
+
 std::vector<process> shortestTaskFirst(std::vector<process> & processes)
 {
     // sort processes vector with lamdba comparator
@@ -104,6 +118,71 @@ std::vector<process> shortestTaskFirst(std::vector<process> & processes)
     });
 
     return processes;
+}
+
+std::vector<process> johnson2machine(std::vector<process> processes)
+{
+    // check condition for 2 machine problem
+    for(auto process : processes)
+    { 
+        if(process.get_time().size() != 2)
+            { throw std::invalid_argument("Invalid argument"); }
+    }
+
+    std::vector<process> result;
+    std::vector<process> result_back;
+    auto sorted_processes = shortestTaskFirst(processes); // get processes on sorted vector
+    
+    for(auto process : sorted_processes)
+    {
+        if(process.get_time().front() < process.get_time().back())
+            { result.push_back(process); }
+        else
+            { result_back.push_back(process); }
+    }
+
+    // insert processes inserted on the back with reverse iterator
+    for(auto iter = result_back.rbegin(); iter != result_back.rend(); ++iter)
+        { result.push_back(*iter); }
+
+    return result;
+}
+
+std::vector<process> johnson3machine(std::vector<process> processes)
+{
+    std::vector<process> result;
+
+    //check condition for 3 machine problem
+    for(auto process : processes)
+    { 
+       if(process.get_time().size() != 3)
+           { throw std::invalid_argument("Invaild argument"); }
+    }
+
+    // virtual times 2D vector
+    std::vector<std::vector<int> > virtual_times(processes.size(),std::vector<int>(2));
+    // create 2 virtual machines in which times in virtual machine 1 are summed times
+    // from machine 1 and 2 and in virtual machine 2 times from machine 2 and 3
+    for(unsigned int i=0; i <= virtual_times.size(); ++i)
+    {
+        for(unsigned int j=0; j <= virtual_times.size(); ++j)
+        { virtual_times[i][j] = processes[i].get_time()[j] + processes[i].get_time()[j+1]; }
+    }
+
+    auto virtual_processes = utility::createProcesses(virtual_times);
+    // use johnson algorithm for 2 machine problem
+    auto virtual_order = johnson2machine(virtual_processes);
+    // sort original processes in returned order 
+    for(auto virtual_process : virtual_order)
+    {
+        for(auto process : processes)
+        {
+            if(process.get_id() == virtual_process.get_id())
+                { result.push_back(process); }
+        }
+    }
+
+    return result;
 }
 
 // returns permutation with longest total time of task on every machine
@@ -194,6 +273,28 @@ auto getShortestTaskTimeAccelerated(std::vector<process> & processes, process ta
     return index;
 }
 
+auto getMostExpansiveTask(std::vector<process> & processes)
+{
+    int best_span = std::numeric_limits<int>::max();
+    unsigned int index;
+    process task;
+
+    for(unsigned int i=0; i <= processes.size(); ++i) // loop one element further than processes size
+    {
+        auto current_processes = processes;
+        current_processes.erase(current_processes.begin()+i); 
+        auto span = maxspan(current_processes);
+        // save best span and it's index
+        if(span < best_span)
+            { best_span = span; index = i; task = current_processes[i]; }
+
+        current_processes.clear(); // clear values
+        current_processes.shrink_to_fit(); // deallocate memory
+    }
+
+    return std::make_tuple(index,task);
+}
+
 // implementation of neh schelduing algorithm
 std::vector<process> neh(std::vector<process> & processes)
 {
@@ -227,31 +328,50 @@ std::vector<process> neh_accelerated(std::vector<process> & processes)
     return result;
 }
 
+std::vector<process> neh_modified(std::vector<process> & processes)
+{
+    std::vector<process> result;
+    auto ordered_processes = longestTotalTimeFirstStable(processes);
+    result.push_back(ordered_processes[0]);
+
+    for(unsigned int i=1; i < ordered_processes.size(); ++i)
+    {
+        auto task_index = getShortestTaskTimeAccelerated(result,ordered_processes[i]);
+        result.insert(result.begin()+task_index,ordered_processes[i]);
+        if(i > 2)
+        {
+            auto task_x = getMostExpansiveTask(result);
+            result.erase(result.begin()+ std::get<0>(task_x) );
+            auto task_x_index = getShortestTaskTime(result,std::get<1>(task_x));
+            result.insert(result.begin()+task_x_index,std::get<1>(task_x));
+        }
+    }
+
+    return result;
+}
+
 int main(void)
 {
     auto input = utility::readFile("data.txt");
     auto times = utility::createTimes(input);
     auto processes = utility::createProcesses(times);
-    auto start = std::chrono::high_resolution_clock::now();
-    auto neh_result = neh(processes);
-    auto end = std::chrono::high_resolution_clock::now();
-    auto start2 = std::chrono::high_resolution_clock::now();   
-    auto result_acc = neh_accelerated(processes);
-    auto end2 = std::chrono::high_resolution_clock::now();
-
-    std::chrono::duration<double,std::nano> diff = std::chrono::duration_cast<std::chrono::duration<double,std::nano>> (end-start);
-    std::chrono::duration<double,std::nano> diff2 = std::chrono::duration_cast<std::chrono::duration<double,std::nano>> (end2-start2);
-
-    std::cout << "Neh time: " << diff.count() << " \n";
-    std::cout << "Accelerated time: " << diff2.count() << "\n"; 
-
-    //std::cout << "Neh: " << maxspan(neh_result) << " Time: " << utility::getExecutionTime(neh,processes).count() << "\n";
-    //std::cout << "Accelerated Neh: " << maxspan(result_acc) << " Time: " << utility::getExecutionTime(neh_accelerated,processes).count() << "\n";
-    std::cout << "Neh: " << maxspan(neh_result) << " \n";
-    std::cout << "Accelerated Neh: " << maxspan(result_acc) << "\n";
-
-    for(auto x : neh_result) { std::cout << x.get_id() << " "; } std::cout << "\n";
-    for(auto x : result_acc) { std::cout << x.get_id() << " "; } std::cout << "\n";
     
-    std::cout << "\n";
+    // auto start3 = std::chrono::high_resolution_clock::now();
+    auto neh_time = neh(processes);
+    // auto end3 = std::chrono::high_resolution_clock::now();
+    
+    // auto start4 = std::chrono::high_resolution_clock::now();
+    auto neh_mod = neh_modified(processes);
+    // auto end4 = std::chrono::high_resolution_clock::now();    
+    
+    // std::chrono::duration<double,std::nano> diff3 = std::chrono::duration_cast<std::chrono::duration<double,std::nano>> (end3-start3);
+    // std::chrono::duration<double,std::nano> diff4 = std::chrono::duration_cast<std::chrono::duration<double,std::nano>> (end4-start4);
+
+    // std::cout << "neh time: " << diff3.count() << "\n";
+    // std::cout << "acceleration time: " << diff4.count() << "\n";
+
+    std::cout << "Neh: " << maxspan(neh_time) << "\n";
+    std::cout << "Modified: " << maxspan(neh_mod) << "\n";
+
+    // std::cout << "\n";
 } 
