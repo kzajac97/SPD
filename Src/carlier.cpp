292f702fd3c097d0e9c40550e747538b6618ce29
Krzysztof <kzajac1997@gmail.com>
2019-05-20 19:59:51 +0200

    Update

--- Src/carlier.cpp
+++ Src/carlier.cpp
@@ -4,49 +4,62 @@
 
 std::vector<process> carlier(std::vector<process> & processes)
 {
-    int u = rpq_maxspan(scharge_heap(processes));
+    std::cout << "start Carlier\n";
+    int u = rpq_maxspan(scharge_heap(processes)); 
     auto result = processes;
     int upper_bound = rpq_maxspan(processes);
     int lower_bound = 0;
 
+    std::cout << "Upper bound\n";
+    // if scharge algorthim gives good result
     if(u < upper_bound)
     { 
         upper_bound = u;
-        result = scharge_heap(processes); 
+        result = processes; 
     }
+
+    std::cout << "Get A B C tasks\n";
     // get reference tasks
-    auto a_task = get_a_task(result);
     auto b_task = get_b_task(result);
+    auto a_task = get_a_task(result);
     auto c_task = get_c_task(result);
 
-    if(!std::get<2>(c_task)) // if C task is not found
+    std::cout << "Check C empty\n";
+    // if C task is empty
+    if(!std::get<2>(c_task))
         { return result; }
-    // create K vector
+    
+    std::cout << "Get K vector\n";
+    // get vector from C to B 
     std::vector<process> k_tasks(result.begin() + std::get<1>(c_task),result.begin() + std::get<1>(b_task));
-    // Get R P and Q times for K vector and extract tuple to vectors 
+    // get R P Q times of K vector
+    std::cout << "Get K rpq times\n";
     auto k_rpq_times = get_rpq_times(k_tasks);
     std::vector<int> krtimes = std::get<0>(k_rpq_times);
     std::vector<int> kptimes = std::get<1>(k_rpq_times);
     std::vector<int> kqtimes = std::get<2>(k_rpq_times);
-    // get R and Q times as minimum and P time as sum of all elements
+    // save K vector min times 
+    std::cout << "Get Rk Pk and Qk\n";
     int rk = *std::min_element(krtimes.begin(),krtimes.end()); 
     int qk = *std::min_element(kqtimes.begin(),kqtimes.end());
     int pk = std::accumulate(kptimes.begin(),kptimes.end(),0);
-
+    // if R and P times are greater than C prepare time
+    std::cout << "Set R + P\n";
     if(rk + pk >  result[std::get<1>(c_task)].get_time()[0])
         { result[std::get<1>(c_task)].set_time(rk + pk,0); }
 
-    lower_bound = schrage_pmtn(result);
-
-    // Get R P Q times for K vector with C task inserted    
-    int h_k_tasks = rk + pk + qk;
-    int rkc = rk < std::get<0>(c_task).get_time().front() ? rk : std::get<0>(c_task).get_time().front();
-    int qkc = qk < std::get<0>(c_task).get_time().back() ? rk : std::get<0>(c_task).get_time().back();
-    int pkc = pk + std::get<0>(c_task).get_time()[1];
-   
+    std::cout << "Get lower bound\n";
+    lower_bound = schrage_pmtn(result); // set lower bound to schare with breaks
+    std::cout << "Geh h tasks\n";
+    int h_k_tasks = rk + pk + qk; // set maxspans
+    // Get RPQ times for K vector with C task
+    // get maxspans
+    std::cout << "set rkc pkc and qkc\n";
+    int rkc = std::get<0>(c_task).get_time().front() < rk ? std::get<0>(c_task).get_time().front() : rk;
+    int qkc = std::get<0>(c_task).get_time().back() < rk ? std::get<0>(c_task).get_time().back() : rk;
+    int pkc = std::get<0>(c_task).get_time()[1] + pk;
     int h_kc_tasks = rkc + qkc + pkc;
-
-    // Update bounds
+    std::cout << "set LB\n";
     if(h_k_tasks > lower_bound)
         { lower_bound = h_k_tasks; }
     
@@ -54,37 +67,40 @@ std::vector<process> carlier(std::vector<process> & processes)
         { lower_bound = h_kc_tasks; }
 
     ////
+    std::cout << "LB < UB calier\n";
     if(lower_bound < upper_bound)
         { result = carlier(result); }
-    
+    std::cout << "Set R Time\n";
+    // restore C task prepare time
     result[std::get<1>(c_task)].set_time(std::get<0>(c_task).get_time()[0],0);
-
+    
+    std::cout << "Set time if p+q  > P\n";
     if(qk + pk > result[std::get<1>(c_task)].get_time().back())
         { result[std::get<1>(c_task)].set_time(qk + pk ,0); }
-
+    std::cout << "set lb with scharge pmtn\n";
     lower_bound = schrage_pmtn(result);
-    // update R P Q times for K vector
-    rkc = rk < std::get<0>(c_task).get_time().front() ? rk : std::get<0>(c_task).get_time().front();
-    qkc = qk < std::get<0>(c_task).get_time().back() ? rk : std::get<0>(c_task).get_time().back();
-    pkc = pk + std::get<0>(c_task).get_time()[1];
+    std::cout << "Get Rkc pkq qkc\n";
+    rkc = std::get<0>(c_task).get_time().front() < rk ? std::get<0>(c_task).get_time().front() : rk;
+    qkc = std::get<0>(c_task).get_time().back() < rk ? std::get<0>(c_task).get_time().back() : rk;
+    pkc = std::get<0>(c_task).get_time()[1] + pk;
 
     h_kc_tasks = rkc + qkc + pkc;
-
+    std::cout << "Get H_K times\n";
     if(h_k_tasks > lower_bound)
         { lower_bound = h_k_tasks; }
     
     if(h_kc_tasks > lower_bound)
         { lower_bound = h_kc_tasks; }
 
-    if(lower_bound > upper_bound)
+    if(lower_bound < upper_bound)
         { result = carlier(result); }
-    
+    std::cout << "Set times\n";
     result[std::get<1>(c_task)].set_time(std::get<0>(c_task).get_time().back(),2);
 
     // clear memory
     k_tasks.clear();
     k_tasks.shrink_to_fit();
-
+    std::cout << "End carlier\n";
     return result;
 }
 
