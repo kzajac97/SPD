e205455aa1bc6b93f38258072ee3dc52e8306d24
Krzysztof <kzajac1997@gmail.com>
2019-05-23 22:07:11 +0200

    Add objective carlier

--- Scripts/Carlier.py
+++ Scripts/Carlier.py
@@ -3,204 +3,168 @@ import copy
 from Process import *
 from Schrage import *
 
-def get_b_task(processes):
-    max_value = rpq_maxspan(processes)
-    maxspan = [None] * len(processes)
-    index = 0
+carlier_results = []
+class Carlier:
+    def __init__(self):
+        self.carlier_results = []
+        self.iter = 1
 
-    maxspan[0] = processes[0].times[0] + processes[0].times[1]
-    for i in range(1,len(processes)):
-        if processes[i].times[0] > maxspan[i - 1]:
-            maxspan[i] = processes[i].times[0] + processes[i].times[1]
-        else:
-            maxspan[i] = processes[i].times[1] + maxspan[i-1]
+    def get_b_task(self,processes):
+        max_value = rpq_maxspan(processes)
+        maxspan = [None] * len(processes)
+        index = 0
 
-    for j in range(len(processes)):
-        if maxspan[j] + processes[j].times[2] == max_value:
-            index = j
-
-    return processes[index],index
-
-def get_a_task(processes):
-    max_value = rpq_maxspan(processes)
-    maxspan = [None] * len(processes)
-    index = 0
-    b_task, b_index = get_b_task(processes)
-
-    maxspan[0] = processes[0].times[0] + processes[0].times[1]
-    for i in range(1,len(processes)):
-        if processes[i].times[0] > maxspan[i - 1]:
+        maxspan[0] = processes[0].times[0] + processes[0].times[1]
+        for i in range(1,len(processes)):
+            if processes[i].times[0] > maxspan[i - 1]:
                 maxspan[i] = processes[i].times[0] + processes[i].times[1]
-        else:
-            maxspan[i] = processes[i].times[1] + maxspan[i-1]
+            else:
+                maxspan[i] = processes[i].times[1] + maxspan[i-1]
 
-    j = b_index
-    while j >= 1:
-        if processes[j].times[0] < maxspan[j-1]:
-            index = j
+        for j in range(len(processes)):
+            if maxspan[j] + processes[j].times[2] == max_value:
+                index = j
 
-        j -= 1
+        return processes[index],index
 
-    return processes[index],index
+    def get_a_task(self,processes):
+        max_value = rpq_maxspan(processes)
+        maxspan = [None] * len(processes)
+        index = 0
+        b_task, b_index = self.get_b_task(processes)
 
-def get_c_task(processes):
-    index = 0
-    found = False
+        maxspan[0] = processes[0].times[0] + processes[0].times[1]
+        for i in range(1,len(processes)):
+            if processes[i].times[0] > maxspan[i - 1]:
+                    maxspan[i] = processes[i].times[0] + processes[i].times[1]
+            else:
+                maxspan[i] = processes[i].times[1] + maxspan[i-1]
 
-    _,a_index = get_a_task(processes)
-    _,b_index = get_b_task(processes)
+        j = b_index
+        while j >= 1:
+            if processes[j].times[0] < maxspan[j-1]:
+                index = j
 
-    for i in range(a_index,b_index):
-        if processes[i].times[2] < processes[b_index].times[2]:
-            index = i
-            found = True
+            j -= 1
 
-    if index == a_index or index == b_index:
+        return processes[index],index
+
+    def get_c_task(self,processes):
+        index = 0
         found = False
 
-    return processes[index],index,found
+        _,a_index = self.get_a_task(processes)
+        _,b_index = self.get_b_task(processes)
 
-def carlier(processes):
-    print(rpq_maxspan(processes))
+        for i in range(a_index,b_index):
+            if processes[i].times[2] < processes[b_index].times[2]:
+                index = i
+                found = True
+
+        if index == a_index or index == b_index:
+            found = False
+
+        return processes[index],index,found
+
+    def carlier(self,processes):
+        self.carlier_results.append(rpq_maxspan(processes))
+        if self.iter > 1000:
+            return processes
+
+        self.iter += 1
+
+        u = rpq_maxspan(schrage(processes[:])) 
+        upper_bound = rpq_maxspan(processes)
+        lower_bound = 0
+        optimal_result = processes
     
-    u = rpq_maxspan(schrage(processes[:])) 
-    upper_bound = rpq_maxspan(processes)
-    lower_bound = 0
-    optimal_result = processes
-   
-    if u < upper_bound:
-        upper_bound = u
-        processes = schrage(processes[:])
-    
-    a_task, a_index = get_a_task(processes)
-    b_task, b_index = get_b_task(processes)
-    c_task, c_index,c_found = get_c_task(processes)
+        if u < upper_bound:
+            upper_bound = u
+            processes = schrage(processes[:])
+        
+        a_task, a_index = self.get_a_task(processes)
+        b_task, b_index = self.get_b_task(processes)
+        c_task, c_index,c_found = self.get_c_task(processes)
 
-    if c_found == False:
-        # return processes
-        if rpq_maxspan(processes) < rpq_maxspan(optimal_result):
-            optimal_result = processes
+        if c_found == False:
+            self.carlier_results.append(rpq_maxspan(processes))
+            return processes
+            
+        k_tasks = processes[c_index:b_index]
 
-    k_tasks = processes[c_index:b_index]
+        rk = min([process.times[0] for process in k_tasks])
+        qk = min([process.times[2] for process in k_tasks])
+        pk = sum([process.times[1] for process in k_tasks])
 
-    rk = min([process.times[0] for process in k_tasks])
-    qk = min([process.times[2] for process in k_tasks])
-    pk = sum([process.times[1] for process in k_tasks])
+        if rk+pk > c_task.times[0]:
+            processes[c_index].times[0] = rk + pk
 
-    if rk+pk > c_task.times[0]:
-        processes[c_index].times[0] = rk + pk
+        r_restore = processes[c_index].times[0]
 
-    r_restore = processes[c_index].times[0]
+        processes_copy = copy.deepcopy(processes)
+        lower_bound = schrage_pmtn(processes_copy)
+        
+        h_k_tasks = rk + pk + qk
+        
+        if rk < c_task.times[0]:
+            rkc = rk
+        else:
+            rkc = c_task.times[0]
 
-    #processes_copy = copy.deepcopy(processes)
-    #lower_bound = schrage_pmtn(processes_copy)
-    lower_bound = schrage_pmtn(processes[:])
+        if qk < c_task.times[2]:
+            qkc = qk
+        else:
+            qkc = c_task.times[2]
 
-    h_k_tasks = rk + pk + qk
-    
-    if rk < c_task.times[0]:
-        rkc = rk
-    else:
-        rkc = c_task.times[0]
+        pkc = pk + c_task.times[1]
 
-    if qk < c_task.times[2]:
-        qkc = qk
-    else:
-        qkc = c_task.times[2]
+        h_kc_tasks = rkc + pkc + qkc
 
-    pkc = pk + c_task.times[1]
+        if h_k_tasks > lower_bound:
+            lower_bound = h_k_tasks
+        if h_kc_tasks > lower_bound:
+            lower_bound = h_kc_tasks
 
-    h_kc_tasks = rkc + pkc + qkc
+        if lower_bound < upper_bound:
+            processes = self.carlier(processes)
 
-    if h_k_tasks > lower_bound:
-        lower_bound = h_k_tasks
-    if h_kc_tasks > lower_bound:
-        lower_bound = h_kc_tasks
+        processes[c_index].times[0] = r_restore
 
-    if lower_bound < upper_bound:
-        processes = carlier(processes)
+        if qk + pk > processes[c_index].times[2]:
+            processes[c_index].times[2] = qk + pk
 
-    processes[c_index].times[0] = r_restore
+        q_resotre = processes[c_index].times[2]
 
-    if qk + pk > processes[c_index].times[2]:
-        processes[c_index].times[2] = qk + pk
+        processes_copy = copy.deepcopy(processes)
+        lower_bound = schrage_pmtn(processes_copy)
+        
+        if rk < c_task.times[0]:
+            rkc = rk
+        else:
+            rkc = c_task.times[0]
 
-    q_resotre = processes[c_index].times[2]
+        if qk < c_task.times[2]:
+            qkc = qk
+        else:
+            qkc = c_task.times[2]
 
-    # processes_copy = copy.deepcopy(processes)
-    # lower_bound = schrage_pmtn(processes_copy)
-    lower_bound = schrage_pmtn(processes[:])
+        pkc = pk + c_task.times[1]
 
-    if rk < c_task.times[0]:
-        rkc = rk
-    else:
-        rkc = c_task.times[0]
+        if h_k_tasks > lower_bound:
+            lower_bound = h_k_tasks
 
-    if qk < c_task.times[2]:
-        qkc = qk
-    else:
-        qkc = c_task.times[2]
+        if h_kc_tasks > lower_bound:
+            lower_bound = h_kc_tasks
 
-    pkc = pk + c_task.times[1]
+        if lower_bound < upper_bound:
+            processes = self.carlier(processes)
 
-    if h_k_tasks > lower_bound:
-        lower_bound = h_k_tasks
+        processes[c_index].times[2] = q_resotre
+        
+        self.carlier_results.append(rpq_maxspan(processes))
+        return processes
 
-    if h_kc_tasks > lower_bound:
-        lower_bound = h_kc_tasks
-
-    if lower_bound < upper_bound:
-        processes = carlier(processes)
-
-    processes[c_index].times[2] = q_resotre
-    
-    print('end')
-    return optimal_result
-
-#def carlier(processes):
-#     u = rpq_maxspan(schrage(processes[:]))
-#     ub = rpq_maxspan(processes)
-#     # optimal_result = processes
-
-#     if u < ub:
-#         ub = u 
-#         processes = schrage(processes)
-
-#     a_task, a_index = get_a_task(processes)
-#     b_task, b_index = get_b_task(processes)
-#     c_task, c_index, c_found = get_c_task(processes)
-
-#     if c_found == False:
-#         return processes
-
-#     k_tasks = processes[c_index:b_index]
-
-#     rk = min([process.times[0] for process in k_tasks])
-#     qk = min([process.times[2] for process in k_tasks])
-#     pk = sum([process.times[1] for process in k_tasks])
-
-#     if rk+pk > c_task.times[0]:
-#         processes[c_index].times[0] = rk + pk
-
-#     r_restore = processes[c_index].times[0]
-
-#     lb = schrage_pmtn(processes[:])
-
-#     if lb < ub:
-#         processes = carlier(processes[:])
-
-#     processes[c_index].times[0] = r_restore
-
-#     if qk + pk > processes[c_index].times[2]:
-#         processes[c_index].times[2] = qk + pk
-
-#     q_restore = processes[c_index].times[2]
-
-#     lb = schrage_pmtn(processes[:])
-
-#     if lb < ub:
-#         processes = carlier(processes[:])
-    
-#     processes[c_index].times[2] = q_restore
-
-#     return processes
+    def run_carlier(self,processes):
+        processes = self.carlier(processes)
+        print("OUT")
+        return min(self.carlier_results)
--- Scripts/Main.py
+++ Scripts/Main.py
@@ -5,24 +5,18 @@ from Carlier import *
 from Schrage import *
 
 if __name__ == "__main__":
-    sys.setrecursionlimit(10000)
+    sys.setrecursionlimit(1000000000)
     # files = ["Data/data000.txt","Data/data001.txt","Data/data002.txt",
     #                                     "Data/data003.txt","Data/data004.txt","Data/data005.txt",
     #                                     "Data/data006.txt","Data/data007.txt","Data/data008.txt"]
 
     # files = ["Data/in50.txt","Data/in100.txt","Data/in200.txt"]
-    files = ["Data/data000.txt"]
+    files = ["Data/data007.txt"]
+    Runner = Carlier()
     i = 0
     for f in files:
         data = read_data(f)
         processes = create_processes(data)
-        result = carlier(processes)
-
-        print(rpq_maxspan(result))
+        result = Runner.run_carlier(processes)
         i += 1
-
-    for p in result:
-        for t in p.times:
-            print(t)
-
-        print("\n")    
+        print(result)
