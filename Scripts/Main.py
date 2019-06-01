720c359643c4cc15ce5ec6ec540446c1d0608f36
Krzysztof <kzajac1997@gmail.com>
2019-06-01 13:47:06 +0200

    Add witi

--- Scripts/Main.py
+++ Scripts/Main.py
@@ -7,18 +7,18 @@ from Rpq import *
 from Milp import *
 
 if __name__ == "__main__":
-    files = ["Data/data000.txt","Data/data001.txt","Data/data002.txt",
-            "Data/data003.txt","Data/data004.txt","Data/data005.txt",
-            "Data/data006.txt","Data/data007.txt","Data/data008.txt"]
+	# files = ["Data/data000.txt","Data/data001.txt","Data/data002.txt",
+	#         "Data/data003.txt","Data/data004.txt","Data/data005.txt",
+	#         "Data/data006.txt","Data/data007.txt","Data/data008.txt"]
 
-    # files = ["Data/in50.txt","Data/in100.txt","Data/in200.txt"]
-    # files = ["Data/data001.txt"]
-    for f in files:
-        data = read_data(f)
-        processes = create_processes(data)
-        # MinimizeJobshop(processes)
-        # MinimizeRpqJobshop(processes)
-        # MinimizeRpqMilp(processes)
-        print(f)
-        result = cp_solve(processes)
-        print(result)
+	# files = ["Data/in50.txt","Data/in100.txt","Data/in200.txt"]
+	files = ["Data/witi000.txt"]
+	for f in files:
+		data = read_data(f)
+		processes = create_processes(data)
+		# MinimizeJobshop(processes)
+		# MinimizeRpqJobshop(processes)
+		# MinimizeRpqMilp(processes)
+		print(f)
+		result = Jobshop(processes)
+		print(result)
--- Scripts/Milp.py
+++ Scripts/Milp.py
@@ -88,3 +88,113 @@ def cp_solve(processes):
 
 	# return cmax final value
 	return int(solver.ObjectiveValue())
+
+def relu(x):
+	if x >= 0:
+		return x
+	else:
+		return 0
+
+def witi(processes):
+	horizon = sum([sum(process.times) for process in processes])
+
+	# solver = pywraplp.Solver('simple_mip_program',pywraplp.Solver.CBC_MIXED_INTEGER_PROGRAMMING)
+	solver = pywraplp.Solver('simple_mip_program',pywraplp.Solver.GLOP_LINEAR_PROGRAMMING)
+	
+	# create varialbes
+	alphas_dict = {}
+	starts = []
+
+	for i in range(len(processes)):
+		for j in range(len(processes)):
+			alphas_dict[i,j] = solver.IntVar(0,1,'alpha_%i_%i' % (i,j))
+		
+	for i in range(len(processes)):
+		starts.append(solver.IntVar(0,horizon,'start_%i' % i))
+
+	# constraints
+	for i in range(len(processes)):
+		solver.Add(starts[i] >= processes[i].times[0])
+		solver.Add(starts[i] + processes[i].times[0] <= processes[i].times[2])
+
+	for i in range(len(processes)):
+		for j in range(i+1, len(processes)):
+			solver.Add(starts[i] + processes[i].times[0] <= starts[j] +
+			alphas_dict[i,j] * horizon)
+
+			solver.Add(starts[j] + processes[j].times[0] <= starts[i]
+			+ alphas_dict[j,i] * horizon)
+
+			solver.Add(alphas_dict[i,j] + alphas_dict[j,i] == 1)
+
+	# solver
+	print('Solving')
+	solver.Parameters = max_time_in_seconds = 120.0
+	solver.Minimize(sum([processes[i].times[1] * (relu(processes[i].times[2] - (starts[i] + processes[i].times[0]))) for i in range(len(processes))]))
+	status = solver.Solve()
+
+	return int(solver.Objective().Value())
+
+def get_jobs(processes):
+	data = []
+	for process in processes:
+		process_data = []
+		for i in range(len(process.times)):
+			process_data.append((i,process.times[i]))
+		data.append(process_data)
+
+	return data
+
+def Jobshop(processes):
+	# Create the model.
+	model = cp_model.CpModel()
+
+	machines_count = len(processes[0].times)
+	all_machines = range(machines_count)
+	jobs_count = len(processes)
+	all_jobs = range(jobs_count)
+
+	jobs_data = get_jobs(processes)
+
+	# maximal value of variables
+	horizon = sum([sum(process.times) for process in processes])
+
+	task_type = collections.namedtuple('task_type', 'start end interval')
+	assigned_task_type = collections.namedtuple('assigned_task_type','start job index')
+
+	# Create jobs.
+	all_tasks = {}
+	for job in range(len(processes)):
+		for task_id, task in enumerate(jobs_data[job]):
+			start_var = model.NewIntVar(0, horizon,'start_%i_%i' % (job, task_id))
+			duration = task[1]
+			end_var = model.NewIntVar(0, horizon, 'end_%i_%i' % (job, task_id))
+			interval_var = model.NewIntervalVar(start_var, duration, end_var, 'interval_%i_%i' % (job, task_id))
+			all_tasks[job, task_id] = task_type(start=start_var, end=end_var, interval=interval_var)
+
+	# Create and add disjunctive constraints.
+	for machine in range(len(processes[0].times)):
+		intervals = []
+		for job in range(len(processes)):
+			for task_id, task in enumerate(jobs_data[job]):
+				if task[0] == machine and job == 1: # only P times can't overlap in this problem
+					intervals.append(all_tasks[job, task_id].interval)
+		# add constraint
+		model.AddNoOverlap(intervals)
+
+	# Add precedence contraints.
+	for job in all_jobs:
+		for task_id in range(0, len(jobs_data[job]) - 1):
+			model.Add(all_tasks[job, task_id + 1].start >= all_tasks[job, task_id].end)
+
+	# Makespan objective.
+	obj_var = model.NewIntVar(0, horizon, 'makespan')
+	model.AddMaxEquality(obj_var, [all_tasks[(job, len(jobs_data[job]) - 1)].end for job in all_jobs])
+	model.Minimize(obj_var)
+	# Solve model.
+	solver = cp_model.CpSolver()
+	solver.StringParameters = "max_time_in_seconds:120.0"
+	print("Solving")
+	status = solver.Solve(model)
+
+	return int(solver.ObjectiveValue())
